4f1218459d88f47946d073c0b6c03a9f9819e904
Jeffrey Elggren <jeffreyelggren@Alveyworld-11.local>
2015-09-14 10:13:29 -0600

    added this

---
+++ statements.py
@@ -0,0 +1,14 @@
+# learning about if statements
+
+number = int(raw_input("Enter your age"))
+
+if number < 5:
+	print "You are still in middle school"
+elif number < 11:
+	print "You are in elementary school"
+elif number < 15:
+	print "You are still in middle/intermediate school"
+elif number < 19:
+	pring "You are in high school"
+else:
+	pring "You know everything"
